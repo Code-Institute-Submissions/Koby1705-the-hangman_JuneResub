8e35401e1c82d078858de7c77244aef5f7d93d4c
Koby1705 <koby1705@gmail.com>
2022-05-03 16:35:46 +0000

    Define legitimate_word func. passing random_words parameter to obtain desired words

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 # import the random module
 import random
+# from file words.py gets english list of words stored in random_words variable
 from words import random_words
 
-print(random_words)
+
+def legitimate_word(random_words):
+    """
+    Making sure that words from the list with dashes and white spaces are eliminated.
+    Chooses random, valid word from a list and converts all lowercase characters
+    in a string into uppercase characters.
+    """
+    word = random.choice(random_words)
+    while " " or "-" in word:
+        word = random.choice(random_words)
+
+    return word.upper()
